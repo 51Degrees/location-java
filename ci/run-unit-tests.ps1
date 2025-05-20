6c43d52249d5272ad5426b16a63e67d08c3fe10a
Alexander Popel <alexander.popel@postindustria.com>
2025-05-20 17:05:43 +0300

    ci: fix typo

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -1,7 +1,7 @@
 param(
     [Parameter(Mandatory)][string]$RepoName,
     [Parameter(Mandatory)][hashtable]$Keys,
-    [string]$Name,
+    [string]$Name
 )
 
 ./java/run-unit-tests.ps1 -RepoName $RepoName -Name $Name -ExtraArgs "-DSuperResourceKey=$($Keys.TestResourceKey)"
